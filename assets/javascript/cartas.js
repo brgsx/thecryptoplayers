85295860417aafcd75e1132f0bec33a9c5ba91ae
Thiago Borges <tgbprog@gmail.com>
2022-01-25 23:03:58 -0300

    first_commit

---
+++ assets/javascript/cartas.js
@@ -0,0 +1,278 @@
+const objetoCartas = {
+  AngelWisdom: {
+    nome: "Angel Wisdom",
+    atributos: {
+      pace: 85,
+      shooting: 88,
+      passing: 76,
+      dribbling: 80,
+      defending: 40,
+      physical: 84
+    },
+    img:"assets/crypto_imagens/AngelWisdom.png"
+  },
+  AngolaKantor: {
+    nome: "Angola Kantor",
+    atributos: {
+      pace: 79,
+      shooting: 67,
+      passing: 77,
+      dribbling: 84,
+      defending: 88,
+      physical: 84
+    },
+    img:"assets/crypto_imagens/AngolaKantor.png"
+  },
+  Classan: {
+    nome: "Classan",
+    atributos: {
+      pace: 70,
+      shooting: 50,
+      passing: 71,
+      dribbling: 60,
+      defending: 76,
+      physical: 76
+    },
+    img:"assets/crypto_imagens/Classan.png"
+  },
+  Croupier: {
+    nome: "Croupier",
+    atributos: {
+      pace: 73,
+      shooting: 63,
+      passing: 82,
+      dribbling: 77,
+      defending: 80,
+      physical: 72
+    },
+    img:"assets/crypto_imagens/Croupier.png"
+  },
+  DanyElmo: {
+    nome: "Dany Elmo",
+    atributos: {
+      pace: 67,
+      shooting: 76,
+      passing: 81,
+      dribbling: 86,
+      defending: 50,
+      physical: 60
+    },
+    img:"assets/crypto_imagens/DanyElmo.png"
+  },
+  DiMarino: {
+    nome: "Di Marino",
+    atributos: {
+      pace: 83,
+      shooting: 81,
+      passing: 86,
+      dribbling: 87,
+      defending: 48,
+      physical: 69
+    },
+    img:"assets/crypto_imagens/DiMarino.png"
+  },
+  Dolby: {
+    nome: "Dolby",
+    atributos: {
+      pace: 73,
+      shooting: 81,
+      passing: 69,
+      dribbling: 77,
+      defending: 30,
+      physical: 68
+    },
+    img:"assets/crypto_imagens/Dolby.png"
+  },
+  DonavanBeck: {
+    nome: "Donavan Beck",
+    atributos: {
+      pace: 68,
+      shooting: 78,
+      passing: 76,
+      dribbling: 79,
+      defending: 72,
+      physical: 77
+    },
+    img:"assets/crypto_imagens/DonavanBeck.png"
+  },
+  Gentleman: {
+    nome: "Gentleman",
+    atributos: {
+      pace: 63,
+      shooting: 79,
+      passing: 86,
+      dribbling: 80,
+      defending: 72,
+      physical: 77
+    },
+    img:"assets/crypto_imagens/Gentleman.png"
+  },
+  HeroInsane: {
+    nome: "Hero Insane",
+    atributos: {
+      pace: 93,
+      shooting: 85,
+      passing: 83,
+      dribbling: 89,
+      defending: 42,
+      physical: 76
+    },
+    img:"assets/crypto_imagens/HeroInsane.png"
+  },
+  Hulliguer: {
+    nome: "Hulliguer",
+    atributos: {
+      pace: 75,
+      shooting: 43,
+      passing: 68,
+      dribbling: 64,
+      defending: 84,
+      physical: 84
+    },
+    img:"assets/crypto_imagens/Hulliguer.png"
+  },
+  IanSong: {
+    nome: "Ian Song",
+    atributos: {
+      pace: 50,
+      shooting: 60,
+      passing: 61,
+      dribbling: 58,
+      defending: 80,
+      physical: 70
+    },
+    img:"assets/crypto_imagens/IanSong.png"
+  },
+  IvoPierseat: {
+    nome: "Ivo Pierseat",
+    atributos: {
+      pace: 81,
+      shooting: 79,
+      passing: 71,
+      dribbling: 79,
+      defending: 64,
+      physical: 74
+    },
+    img:"assets/crypto_imagens/IvoPierseat.png"
+  },
+  KaosanAlHayder: {
+    nome: "Kaosan AlHayder",
+    atributos: {
+      pace: 83,
+      shooting: 80,
+      passing: 71,
+      dribbling: 68,
+      defending: 40,
+      physical: 73
+    },
+    img:"assets/crypto_imagens/KaosanAlHayder.png"
+  },
+  Lumeluku: {
+    nome: "Lumeluku",
+    atributos: {
+      pace: 85,
+      shooting: 88,
+      passing: 76,
+      dribbling: 80,
+      defending: 40,
+      physical: 84
+    },
+    img:"assets/crypto_imagens/Lumeluku.png"
+  },
+  MarkWinOz: {
+    nome: "Mark WinOz",
+    atributos: {
+      pace: 81,
+      shooting: 53,
+      passing: 75,
+      dribbling: 74,
+      defending: 89,
+      physical: 81
+    },
+    img:"assets/crypto_imagens/MarkWinOz.png"
+  },
+  Mind: {
+    nome: "Mind",
+    atributos: {
+      pace: 74,
+      shooting: 54,
+      passing: 76,
+      dribbling: 76,
+      defending: 75,
+      physical: 76
+    },
+    img:"assets/crypto_imagens/Mind.png"
+  },
+  Mountain: {
+    nome: "Mountain",
+    atributos: {
+      pace: 74,
+      shooting: 79,
+      passing: 84,
+      dribbling: 83,
+      defending: 55,
+      physical: 65
+    },
+    img:"assets/crypto_imagens/Mountain.png"
+  },
+  NitroVik: {
+    nome: "Nitro Vik",
+    atributos: {
+      pace: 72,
+      shooting: 82,
+      passing: 62,
+      dribbling: 78,
+      defending: 30,
+      physical: 90
+    },
+    img:"assets/crypto_imagens/NitroVik.png"
+  },
+  Schakalaka: {
+    nome: "Schakalaka",
+    atributos: {
+      pace: 72,
+      shooting: 82,
+      passing: 62,
+      dribbling: 78,
+      defending: 30,
+      physical: 90
+    },
+    img:"assets/crypto_imagens/Schakalaka.png"
+  },
+  Schmasgard: {
+    nome: "Schmasgard",
+    atributos: {
+      pace: 84,
+      shooting: 78,
+      passing: 80,
+      dribbling: 47,
+      defending: 90,
+      physical: 83
+    },
+    img:"assets/crypto_imagens/Schmasgard.png"
+  },
+  Tuche: {
+    nome: "Tuche",
+    atributos: {
+      pace: 69,
+      shooting: 53,
+      passing: 72,
+      dribbling: 71,
+      defending: 78,
+      physical: 76
+    },
+    img:"assets/crypto_imagens/Tuche.png"
+  }, 
+  VanDijsel: {
+    nome: "Van Dijsel",
+    atributos: {
+      pace: 80,
+      shooting: 61,
+      passing: 72,
+      dribbling: 73,
+      defending: 92,
+      physical: 86
+    },
+    img:"assets/crypto_imagens/VanDijsel.png"
+  }
+};
---
+++ assets/javascript/script.js
@@ -0,0 +1,232 @@
+const screen = document.getElementById('screen');
+const objPlayers = {
+  player: {
+    cards: '',
+    points: 0,
+    amountCards: 0
+  },
+  pc: {
+    cards: '',
+    points: 0
+  },
+  returnImagen: function(player){
+    return this[player].cards.img;
+  },
+  returnAtributs: function(player, atribut) {
+    return this[player].cards.atributos[atribut];
+  }
+};
+
+// Menu principal 
+const initialOptions = () => {
+  const div = document.createElement('div');
+  const span = document.createElement('span');
+  const select = document.createElement('select');
+  const options1 = document.createElement('option');
+  const options2 = document.createElement('option');
+  const options3 = document.createElement('option');
+  const button = document.createElement('button');
+
+  div.className = 'initiDiv';
+  span.innerText = 'Deck with:';
+  select.className = 'game-options';
+  select.id = 'numberCards'
+  options1.innerText = '11 Cards';
+  options1.value = '11';
+  button.innerText = 'Start Game';
+  button.id = 'startGame';
+  button.className = 'button';
+
+  select.appendChild(options1);
+  div.appendChild(span);
+  div.appendChild(select);
+  div.appendChild(button);
+  screen.appendChild(div);
+};
+
+const functionRoutes = (event) => {
+  const clickedTarget = event.target;
+  const clickedTargetId = clickedTarget.id;
+
+
+  if (clickedTargetId === "startGame") {
+    amountOfCards(objPlayers);
+    cleanScreen();
+    startGame();
+    setWaitingTime(500, drawLetter, objetoCartas);
+    addInputAtributs();
+  };
+  if(clickedTarget.name === "atributo"){
+    // changeClassInputAttributes('add')
+    setWaitingTime(200, playerAttribute, {clickedTargetId, objPlayers});
+    setWaitingTime(200, showCardPc, objPlayers);
+    setWaitingTime(200, showScore);
+  };
+};
+
+const amountOfCards = ({player}) => {
+  const number = document.getElementById('numberCards');
+  player.amountCards = Number(number.value);
+};
+
+const startGame = () => {
+  const scoreboard = document.createElement('div');
+  const scoreboardText = document.createElement('h2');
+  const winner = document.createElement('di');
+  const cardPc = document.createElement('div');
+  const cardPlayer = document.createElement('div');
+  const imgCardPc = document.createElement('img');
+  const imgCardPlayer = document.createElement('img');
+
+  scoreboardText.innerHTML = ' 0 X 0 ';
+  scoreboard.className = 'scoreboard';
+  winner.className = 'winner';
+  cardPc.className = 'div-cards';
+  cardPc.id = 'card-pc';
+  cardPlayer.className = 'div-cards';
+  cardPlayer.id = 'card-player';
+  imgCardPc.src = 'assets/crypto_imagens/fundo.jpg';
+  imgCardPc.className = 'cards';
+  imgCardPlayer.src = 'assets/crypto_imagens/fundo.jpg';
+  imgCardPlayer.className = 'cards';
+
+  scoreboard.appendChild(scoreboardText);
+  scoreboard.appendChild(winner);
+  cardPc.appendChild(imgCardPc);
+  cardPlayer.appendChild(imgCardPlayer);
+  screen.appendChild(scoreboard);
+  screen.appendChild(cardPlayer);
+  screen.appendChild(cardPc);
+};
+
+const showScore = () => {
+  const scoreboard = document.querySelector(".scoreboard h2");
+  scoreboard.innerText = ` ${objPlayers.player.points} X ${objPlayers.pc.points} `;
+  
+};
+
+const random = (maxValue) => {
+  return parseInt(Math.random() * maxValue);
+}
+
+const returnLetters = (objetoCards) => {
+  const nameCards = Object.keys(objetoCards);
+  let index = random(nameCards.length);
+  let indexCard = nameCards[index];
+  return objetoCards[indexCard];
+};
+
+const drawLetter = (objetoCards) => {
+  objPlayers.pc.cards = returnLetters(objetoCards);
+
+  objPlayers.player.cards = returnLetters(objetoCards);
+
+  displaysPlayerCard();
+};
+
+function displaysPlayerCard() {
+  const imgCardPlayer = document.querySelector("#card-player .cards");
+  imgCardPlayer.src = objPlayers.returnImagen('player');
+  animationEffects(500, imgCardPlayer, 'flip');
+
+  changeClassInputAttributes('remove');
+}
+
+const addInputAtributs = () => {
+  const divCardPlayer = document.querySelector("#card-player");
+  const attributesValue = ["pace", "dribbling", "shooting", "defending", "passing", "physical"];
+  const divInputs = document.createElement('div');
+
+  for (let i = 0; i < 6; i++) {
+      const input = document.createElement('input');
+      input.type = 'button';
+      input.name = 'atributo';
+      input.id = attributesValue[i];
+      input.className = attributesValue[i];
+      divInputs.append(input);
+  };
+
+  divInputs.classList.add("cards-status");
+  divCardPlayer.appendChild(divInputs);
+}
+
+function playerAttribute({clickedTargetId, objPlayers}) {
+  const loseAudio = new Audio('assets/music/boo.mp3');
+  const winAudio = new Audio('assets/music/palmas2.mp3');
+  const divCards = document.getElementsByClassName("cards");
+  const vencedor = document.querySelector(".winner");
+  const playerSelectedAttribute = objPlayers.returnAtributs('player',clickedTargetId);
+  const pcSelectedAttribute = objPlayers.returnAtributs('pc',clickedTargetId);
+  let playerWinner = 'TIE';
+
+  if (playerSelectedAttribute > pcSelectedAttribute) {
+    winAudio.play();
+    playerWinner = "You win";
+    objPlayers.player.points++;
+    animationEffects(1100, divCards[0], 'winner');
+  }
+  if (playerSelectedAttribute < pcSelectedAttribute) {
+    loseAudio.play();
+    playerWinner = "You lose";
+    objPlayers.pc.points++;
+    animationEffects(1100, divCards[1], 'winner');
+  }
+  vencedor.innerHTML = `<h2>${playerWinner}</h2>`;
+}
+
+const showCardPc = (objPlayers) => {
+  const divCards = document.querySelector("#card-pc .cards");
+  animationEffects(500, divCards, 'flip');
+  divCards.src = objPlayers.returnImagen('pc');
+  setWaitingTime(5000, turnCard);
+}
+
+const turnCard = () => {
+  const divCards = document.getElementsByClassName("cards");
+  divCards[0].src = 'assets/crypto_imagens/fundo.jpg';
+  animationEffects(500, divCards[0], 'flip');
+  divCards[1].src = 'assets/crypto_imagens/fundo.jpg';
+  animationEffects(500, divCards[1], 'flip');
+  changeClassInputAttributes('add')
+  objPlayers.player.amountCards--;
+
+  if(objPlayers.player.amountCards > 0){
+    setWaitingTime(600, drawLetter, objetoCartas);
+  }else {
+    reset(objPlayers);
+  }
+}
+
+const setWaitingTime = (time, callBack, value) =>{
+  setTimeout(() => callBack(value), time);
+};
+
+const reset = (objPlayers) => {
+  cleanScreen();
+
+  objPlayers.player.cards = [];
+  objPlayers.player.index = 0;
+  objPlayers.player.points = 0;
+
+  objPlayers.pc.cards = [];
+  objPlayers.pc.index = 0;
+  objPlayers.pc.points = 0;
+  initialOptions();
+}
+
+const cleanScreen = () => {
+  screen.innerText = ''
+}
+
+const animationEffects = (time, imgCard, className) => {
+  imgCard.classList.add(className);
+  setTimeout(() => imgCard.classList.remove(className), time);
+}
+
+const changeClassInputAttributes = (clas) =>{
+  const divInputs = document.querySelector('.cards-status');
+  divInputs.classList[clas]('hidden');
+}
+
+initialOptions();
+screen.addEventListener("click", functionRoutes);
